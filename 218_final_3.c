2c809bff06cd4d45da5726802fa7ccfdd335f58a
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-29 19:20:47 +0700

    add

--- 218_final_3.c
+++ 218_final_3.c
@@ -5,36 +5,47 @@ int main()
     scanf("%d %d %d",&n,&m,&p);
     if(n<=20&&m<=20&&p<=20)
     {
-        int a[n][n],b[m][m],c[p][p],u[n],t[m],s[p],d=0,e=0,f=0;
+        int a[n][n],b[m][m],c[p][p],u[n],t[n],s=0,s1=0,d=1,e=0,f=0,k;
         printf("\n\n");
+        k=n-1;
         for(i=0; i<n; i++)
         {
             u[i]=0;
+            t[i]=0;
             for(j=0; j<n; j++)
             {
                 scanf("%d",&a[i][j]);
-                u[i]+=a[i][j];
             }
         }
-        for(i=1; i<n; i++)
+        for(i=0; i<n; i++)
         {
-            if(u[0]==u[i])
+            for(j=0; j<n; j++)
+            {
+                u[i]+=a[i][j];
+                t[i]+=a[j][i];
+            }
+            s+=a[i][i];
+            s1+=a[i][k];
+            k--;
+        }
+        for(i=0; i<n; i++)
+        {
+            if(u[i]==t[i]&&u[i]==s&&u[i]==s1)
             {
                 d=1;
+            }
+            else
+            {
+                d=0;
+                break;
             }
         }
         if(d==1)
         {
-            for(j=0; j<n; j++)
-            {
-                u[j]=0;
-                for(i=0; i<n; i++)
-                {
-                    u[j]+=a[i][j];
-                }
-                printf("%d ",u[j]);
-            }
+            printf("No ");
         }
+        else
+            printf("Yes ");
         /*for(i=0;i<m;i++)
         {
             for(j=0;j<m;j++)
